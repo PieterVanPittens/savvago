cf1bd0faed255d6f243e769965c5245ef4644d2e
PieterVanPittens <pieter.van.pittens@outlook.com>
2017-05-18 21:19:11 +0200

    repaired apps

--- setup/content.sql
+++ setup/content.sql
@@ -3,11 +3,11 @@
 -- Dumping data for table `apps`
 --
 
-INSERT INTO `apps` (`app_id`, `name`, `title`, `description`, `is_active`) VALUES
-(1, 'app-management', 'Apps', 'Manage Apps', 1),
-(2, 'user-management', 'Users', 'Manage Users', 1),
-(3, 'lesson-management', 'Lessons', 'Manage Lessons, assign Roles', 1),
-(4, 'journey-management', 'Journeys', 'Manage journeys', 1);
+INSERT INTO `apps` (`app_id`, `name`, `title`, `description`, `is_active`, `role_id`) VALUES
+(1, 'app-management', 'Apps', 'Manage Apps', 1, 4),
+(2, 'user-management', 'Users', 'Manage Users', 1, 4),
+(3, 'lesson-management', 'Lessons', 'Manage Lessons, assign Roles', 1, 3),
+(4, 'journey-management', 'Journeys', 'Manage journeys', 1, 3);
 
 --
 -- Dumping data for table `content_types`
