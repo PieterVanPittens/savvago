276f9f058ab545891d4fc1b5f2fa8018e715222f
PieterVanPittens <pieter.van.pittens@outlook.com>
2017-05-22 09:45:26 +0200

    add multiple lessons to journey

--- tests/api/CreateJourneyCept.php
+++ tests/api/CreateJourneyCept.php
@@ -5,7 +5,7 @@ $I->wantTo('Create a journey');
 login($I, "admin@domain.de","MakeMeRandomAndSecure");
 
 // create journey
-$journey = ["title"=> getUUID(), "tags"=>"tag1 tag2", "description"=>"hallo was geht"];
+$journey = ["title"=> getUUID(), "tags"=>"tag5 tag6", "description"=>"hallo was geht"];
 $I->sendPost('journeys', json_encode($journey));
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
@@ -18,16 +18,35 @@ $I->sendPost('journeys/' . $journey->journeyId, json_encode($activate));
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 
-// create lessons
-$lesson = ["title"=> getUUID(), "tags"=>"tag1 tag2", "type"=>"jpg"];
-$I->sendPost('lessons', json_encode($lesson));
-$I->seeResponseCodeIs(200);
-$I->seeResponseIsJson();
-$response = json_decode($I->grabResponse());
-$lesson = $response->object;
+// create jpg lessons
+for ($i = 1; $i <= 5; $i++) {
+	$lesson = ["title"=> $i . " - " . getUUID(), "tags"=>"tag6 tag5", "type"=>"jpg"];
+	$I->sendPost('lessons', json_encode($lesson));
+	$I->seeResponseCodeIs(200);
+	$I->seeResponseIsJson();
+	$response = json_decode($I->grabResponse());
+	$lesson = $response->object;
+	\PHPUnit_Framework_Assert::assertSame(1, $response->message->type);
+	// activate lesson
+	$activate = ["isActive" => "1"];
+	$I->sendPost('lessons/' . $lesson->lessonId, json_encode($activate));
+	$I->seeResponseCodeIs(200);
+	$I->seeResponseIsJson();
+}
 
-// activate lesson
-$activate = ["isActive" => "1"];
-$I->sendPost('lessons/' . $lesson->lessonId, json_encode($activate));
-$I->seeResponseCodeIs(200);
-$I->seeResponseIsJson();
+// create yt lessons
+for ($i = 1; $i <= 10; $i++) {
+	$lesson = ["title"=> $i . " - " . getUUID(), "tags"=>"tag5 tag6", "type"=>"youtube", "link" => "https://www.youtube.com/watch?v=5v5YqE__Nco"];
+	$I->sendPost('lessons', json_encode($lesson));
+	$I->seeResponseCodeIs(200);
+	$I->seeResponseIsJson();
+	$response = json_decode($I->grabResponse());
+	$lesson = $response->object;
+	\PHPUnit_Framework_Assert::assertSame(1, $response->message->type);
+
+	// activate lesson
+	$activate = ["isActive" => "1"];
+	$I->sendPost('lessons/' . $lesson->lessonId, json_encode($activate));
+	$I->seeResponseCodeIs(200);
+	$I->seeResponseIsJson();
+}
